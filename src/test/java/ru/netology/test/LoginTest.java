478264ab065cb69cb351b2a6e276011cc7a77fec
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2021-12-01 22:13:46 +0300

    New Project version

---
+++ src/test/java/ru/netology/test/LoginTest.java
@@ -0,0 +1,92 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import lombok.var;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.netology.testmode.DataGenerator;
+
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.testmode.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.testmode.DataGenerator.Registration.getUser;
+
+
+class LoginTest {
+
+    @BeforeEach
+    void setup() {
+        Configuration.headless = true;
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+
+        DataGenerator.RegistrationDto registeredUser = getRegisteredUser("active");
+        $("[name=login]").setValue(registeredUser.getLogin());
+        $("[name=password]").setValue(registeredUser.getPassword());
+        $(".button__text").click();
+        $(".App_appContainer__3jRx1 h2.heading").should(exist, Duration.ofSeconds(5000));
+        $(".App_appContainer__3jRx1 h2.heading").shouldHave(matchText("Личный кабинет"));
+    }
+
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        DataGenerator.RegistrationDto notRegisteredUser = getUser ("active");
+        $("[name=login]").setValue(notRegisteredUser.getLogin());
+        $("[name=password]").setValue(notRegisteredUser.getPassword());
+        $(".button__text").click();
+        $(".notification_status_error").shouldBe(visible, Duration.ofSeconds(5000));
+        $("[data-test-id='error-notification']").shouldHave(Condition.text("Неверно указан логин или пароль"))
+                .shouldBe(Condition.visible);
+
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        DataGenerator.RegistrationDto blockedUser = getRegisteredUser("blocked");
+        $("[name=login]").setValue(blockedUser.getLogin());
+        $("[name=password]").setValue(blockedUser.getPassword());
+        $(".button__text").click();
+        $(".notification_status_error").shouldBe(visible, Duration.ofSeconds(5000));
+        $(".notification_visible[data-test-id=error-notification]").shouldHave(Condition.matchText("Ошибка! Пользователь заблокирован"));
+
+
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        DataGenerator.RegistrationDto registeredUser = getRegisteredUser("active");
+        String wrongLogin = DataGenerator.getLogin();
+        $("[name=login]").setValue(wrongLogin);
+        $("[name=password]").setValue(registeredUser.getPassword());
+        $(".button__text").click();
+        $(".notification_status_error").shouldBe(visible, Duration.ofSeconds(5000));
+        $(".notification_visible[data-test-id=error-notification]").shouldHave(Condition.matchText("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        DataGenerator.RegistrationDto registeredUser = getRegisteredUser("active");
+        String wrongPassword = DataGenerator.getPassword();
+        $("[name=login]").setValue(registeredUser.getLogin());
+        $("[name=password]").setValue(wrongPassword);
+        $(".button__text").click();
+        $(".notification_status_error").shouldBe(visible, Duration.ofSeconds(5000));
+        $(".notification_visible[data-test-id=error-notification]").shouldHave(Condition.matchText("Ошибка! Неверно указан логин или пароль"));
+
+    }
+}
---
+++ src/test/java/ru/netology/testmode/DataGenerator.java
@@ -0,0 +1,75 @@
+package ru.netology.testmode;
+
+import com.github.javafaker.Faker;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+import lombok.val;
+
+import java.util.Locale;
+
+
+public class DataGenerator {
+
+    private static final RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private DataGenerator() {
+    }
+
+    private static void sendRequest(RegistrationDto user) {
+        RestAssured.given()
+                .spec(requestSpecification)
+                .body(user)
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+    }
+
+    public static String getLogin() {
+        String login = faker.name().firstName();
+        return login;
+    }
+
+    public static String getPassword() {
+        String password = faker.internet().password();
+        return password;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+
+        public static RegistrationDto getUser(String status) {
+            RegistrationDto user = new RegistrationDto(getLogin(),getPassword(), status);
+            return user;
+        }
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            RegistrationDto registeredUser = getUser(status);
+            DataGenerator.sendRequest(registeredUser);
+            return registeredUser;
+        }
+    }
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+
+}
+
